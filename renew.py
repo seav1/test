2ac76506d28a3aba5a4b2548f0c019847256dee3
seav1 <107227866+seav1@users.noreply.github.com>
2023-09-08 16:04:33 +0800

    Update renew.py

--- renew.py
+++ renew.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-API_ENDPOINT = "https://1833107456.kube.api.section.io"
+API_ENDPOINT = "url = "https://aperture.section.io/api/v1/accounts/me/sites/all/hibernation"
 
 # 指定需要进行续期操作的节点 ID 和天数
 NODE_ID = 43009
